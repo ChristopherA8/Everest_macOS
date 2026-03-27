1549f123091c34c9640125af6d68ea6bb19d548e
ChristopherA8 <christopheranderson3@me.com>
2026-03-27 08:14:06 +0100

    [Tweak/Source] Add settings

--- tweak/tweak.m
+++ tweak/tweak.m
@@ -2,14 +2,22 @@
 #import <QuartzCore/QuartzCore.h>
 #import <objc/runtime.h>
 
+static NSString *plistPath;
+
 @interface DockBar
 -(void)appLaunchedForTile:(id)tile;
 @end
 
 static void (*orig_handleDockEventAppLaunch)(id self, SEL _cmd, id tile, BOOL alreadyExists, BOOL appLaunch, void *context);
 
-static void hook_handleDockEventAppLaunch_noAnim(id self, SEL _cmd, id tile, BOOL alreadyExists, BOOL appLaunch, void *context) {
+static void hook_handleDockEventAppLaunch(id self, SEL _cmd, id tile, BOOL alreadyExists, BOOL appLaunch, void *context) {
     NSLog(@"[Tweak] App icon pressed tile=%@", tile);
+
+    NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:plistPath];
+    if ([dict[@"Enabled"] isEqualToNumber:@0]) {
+        orig_handleDockEventAppLaunch(self, _cmd, tile, alreadyExists, appLaunch, context);
+        return;
+    };
 
     // Launch the app
     if (appLaunch) {
@@ -26,25 +34,37 @@ static void hook_handleDockEventAppLaunch_noAnim(id self, SEL _cmd, id tile, BOO
     }
 
     if (layer) {
-        CAKeyframeAnimation *bounce = [CAKeyframeAnimation animationWithKeyPath:@"transform.translation.y"];
-        bounce.values = @[@0, @-10, @10, @-5, @5, @0];
-        bounce.keyTimes = @[@0, @0.2, @0.4, @0.6, @0.8, @1];
-        bounce.duration = 0.6;
-        bounce.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+        if ([dict[@"Animation"] isEqualToNumber:@1]) {
+            CAKeyframeAnimation *bounce = [CAKeyframeAnimation animationWithKeyPath:@"transform.translation.y"];
+            bounce.values = @[@0, @-10, @10, @-5, @5, @0];
+            bounce.keyTimes = @[@0, @0.2, @0.4, @0.6, @0.8, @1];
+            bounce.duration = 0.6;
+            bounce.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
 
-        CAKeyframeAnimation *scale = [CAKeyframeAnimation animationWithKeyPath:@"transform.scale"];
-        scale.values = @[@1.0, @1.1, @0.95, @1.05, @1.0];
-        scale.keyTimes = @[@0, @0.2, @0.5, @0.8, @1];
-        scale.duration = 0.6;
-        scale.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+            CAKeyframeAnimation *scale = [CAKeyframeAnimation animationWithKeyPath:@"transform.scale"];
+            scale.values = @[@1.0, @1.1, @0.95, @1.05, @1.0];
+            scale.keyTimes = @[@0, @0.2, @0.5, @0.8, @1];
+            scale.duration = 0.6;
+            scale.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
 
-        CAAnimationGroup *group = [CAAnimationGroup animation];
-        group.animations = @[bounce, scale];
-        group.duration = 0.6;
-        group.fillMode = kCAFillModeForwards;
-        group.removedOnCompletion = YES;
+            CAAnimationGroup *group = [CAAnimationGroup animation];
+            group.animations = @[bounce, scale];
+            group.duration = 0.6;
+            group.fillMode = kCAFillModeForwards;
+            group.removedOnCompletion = YES;
 
-        [layer addAnimation:group forKey:@"customBounce"];
+            [layer addAnimation:group forKey:@"customBounce"];
+        } else if ([dict[@"Animation"] isEqualToNumber:@2]) {
+            CAKeyframeAnimation *scale = [CAKeyframeAnimation animationWithKeyPath:@"transform.scale"];
+            scale.values = @[@1.0, @1.1, @0.95, @1.05, @1.0];
+            scale.keyTimes = @[@0, @0.2, @0.5, @0.8, @1];
+            scale.duration = 0.6;
+            scale.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+            scale.fillMode = kCAFillModeForwards;
+            scale.removedOnCompletion = YES;
+
+            [layer addAnimation:scale forKey:@"customScale"];
+        }
     }
 
     #pragma mark - future work
@@ -54,9 +74,35 @@ static void hook_handleDockEventAppLaunch_noAnim(id self, SEL _cmd, id tile, BOO
 
 __attribute__((constructor))
 static void tweak_init() {
+    // If plist doesn't exist create it
+    NSString *path = @"~/Library/Application Support/EverestSettings/settings.plist";
+    plistPath = [path stringByExpandingTildeInPath];
+
+    // Ensure the folder exists
+    NSString *dir = [plistPath stringByDeletingLastPathComponent];
+    NSError *error = nil;
+    [[NSFileManager defaultManager] createDirectoryAtPath:dir
+                              withIntermediateDirectories:YES
+                                               attributes:nil
+                                                    error:&error];
+    if (error) {
+        NSLog(@"Failed to create Application Support folder: %@", error);
+    }
+
+    // If file is empty, setup default values
+    NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:plistPath];
+    if (!dict) {
+        NSDictionary *dict = @{
+            @"Enabled": @1,
+            @"Animation": @0
+        };
+        [dict writeToFile:plistPath atomically:YES];
+    }
+
+    // Start swizzling
     Class cls = objc_getClass("DockBar");
     SEL sel = @selector(handleDockEventAppLaunch:alreadyExists:appLaunch:context:);
     Method m = class_getInstanceMethod(cls, sel);
-    // orig_handleDockEventAppLaunch = (void *)method_getImplementation(m);
-    method_setImplementation(m, (IMP)hook_handleDockEventAppLaunch_noAnim);
+    orig_handleDockEventAppLaunch = (void *)method_getImplementation(m);
+    method_setImplementation(m, (IMP)hook_handleDockEventAppLaunch);
 }
